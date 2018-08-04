3dd2d3e2495135c852e8e02e9a6a98775fc26a14
SaraMagdy94 <30289008+SaraMagdy94@users.noreply.github.com>
2018-08-04 02:23:24 +0200

    Add files via upload

---
+++ src/placeLocations.js
@@ -0,0 +1,48 @@
+
+export const locations =
+
+    [
+        {
+            title: '  Zohour Flower Shop (زهور فلاور شوب)  ',
+            location: { lat: 30.04518, lng: 31.240486 } 
+        },
+
+        { 
+            title: 'Fancy Flower     (فانسى فلاور) ',
+            location: { lat: 30.067405530805004, lng: 31.20379444234767 }
+         },
+
+        { 
+            title: 'Rosa Flowers ', 
+            location: { lat: 30.053489945141326, lng: 31.205499776401425 } 
+        },
+        {
+            title: 'Beauty Flower',
+            location: { lat: 30.0596185, lng: 31.3285051 }
+        },
+        {
+            title: 'Flower Fish',
+            location: { lat: 30.061328887939453, lng: 31.21465301513672 }
+        },
+        {
+            title: 'pergola flower shop ',
+            location: { lat: 30.060621895153822, lng: 31.34569446397231 }
+        }, 
+        {
+            title: 'The Red Flower ',
+            location: { lat: 30.045860290527344, lng: 31.214584350585938 }
+        }, 
+        {
+            title: 'Flower Market ',
+            location: { lat: 30.081897, lng: 31.338409 }
+        },
+        {
+            title: 'Salomy Flowers ',
+            location: { lat: 30.05180084852355, lng: 31.194154650083444 }
+        }, 
+        {
+            title: 'Flower Center ',
+            location: { lat: 30.087701699745537, lng: 31.344231300959187}
+        }
+       
+    ]
